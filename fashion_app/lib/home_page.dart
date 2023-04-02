3f1915c32abbd09002ba1dead0379ed6a649172e
Saw-YanLinOo <yly80103@gmail.com>
2023-04-02 15:21:26 +0630

    add trending for you

--- fashion_app/lib/home_page.dart
+++ fashion_app/lib/home_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    print("screen height :: ${MediaQuery.of(context).size.height / 2}");
+    print("screen width :: ${MediaQuery.of(context).size.width}");
+
     return SafeArea(
       child: Scaffold(
         body: Column(
@@ -14,80 +18,188 @@ class HomePage extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: Row(
-                children: [
-                  Container(
-                    width: 42,
-                    height: 42,
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                    ),
-                    child: Image.asset(
-                      "assets/profile.png",
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Hawdy",
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                      Text(
-                        "Christia Yota",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Spacer(),
-                  Icon(
-                    Icons.notifications,
-                    size: 32,
-                  ),
-                ],
-              ),
+              child: HomeAppBarSection(),
             ),
             SizedBox(
               height: 24,
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                children: [
-                  Text(
-                    "Trending For You",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Spacer(),
-                  Text(
-                    "...",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
-                    ),
-                  ),
-                ],
-              ),
+              child: TrendingForYouSection(),
             ),
-            Stack(children: [
-              Image.asset("assets/profile.png"),
-            ],),
           ],
         ),
       ),
     );
   }
 }
+
+class TrendingForYouSection extends StatelessWidget {
+  const TrendingForYouSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              "Trending For You",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Spacer(),
+            Text(
+              "...",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height / 2,
+          clipBehavior: Clip.hardEdge,
+          decoration:
+          BoxDecoration(borderRadius: BorderRadius.circular(16)),
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: Image.asset(
+                  "assets/outfit.jpeg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Positioned(
+                top: 15,
+                right: 15,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.pink.shade100,
+                    shape: BoxShape.circle,
+                  ),
+                  padding: EdgeInsets.all(4),
+                  child: Icon(
+                    FontAwesomeIcons.heart,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 15,
+                left: 15,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "New 2020",
+                      style: TextStyle(
+                        color: Colors.pink.shade100,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: Text(
+                        "Modren Outfit Collection",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          width: 32,
+                          height: 32,
+                          clipBehavior: Clip.hardEdge,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                          ),
+                          child: Image.asset(
+                            "assets/profile.png",
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Text(
+                          "Finar Surat",
+                          style: TextStyle(
+                            color: Colors.pink.shade100,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
+class HomeAppBarSection extends StatelessWidget {
+  const HomeAppBarSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 42,
+          height: 42,
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+          ),
+          child: Image.asset(
+            "assets/profile.png",
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(
+          width: 8,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Hawdy",
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            Text(
+              "Christia Yota",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ],
+        ),
+        Spacer(),
+        Icon(
+          Icons.notifications,
+          size: 32,
+        ),
+      ],
+    );
+  }
+}
+
