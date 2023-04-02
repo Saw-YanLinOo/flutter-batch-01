5f8ac06ab2b778e269c896a083047e8ebd30b8e8
Saw-YanLinOo <yly80103@gmail.com>
2023-04-02 13:07:25 +0630

    fashion app bar

---
+++ fashion_app/android/app/src/main/kotlin/com/next/fashion_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.next.fashion_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ fashion_app/lib/home_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            SizedBox(height: 16,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+              child: Row(
+                children: [
+                  Container(
+                    width: 42,
+                    height: 42,
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                    ),
+                    child: Image.asset(
+                      "assets/profile.png",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(width: 8,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Hawdy",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        "Christia Yota",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  Icon(Icons.notifications,size: 32,),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
